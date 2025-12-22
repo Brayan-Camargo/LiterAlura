6345f42c99be4f071e11cb0e99b30c543d688f11
Brayan-Camargo <brayancamargoramirez@gmail.com>
2025-12-22 00:18:20 -0600

    Se agrego el menu a la clase principal con un while y se adiciono el menu con Scaner utilizando un Switch case. Finalmente se agrego la dependencia al pom de Jackson para el formato Json

--- src/main/java/com/alura/literalura/principal/Principal.java
+++ src/main/java/com/alura/literalura/principal/Principal.java
@@ -1,4 +1,53 @@
 package com.alura.literalura.principal;
 
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.Scanner;
+
 public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+
+    public void muestraMenu(){
+        var opcion = -1;
+        while (opcion != 0){
+            var menu = """
+                    1 - Buscar libro por titulo
+                    2 - Enlistar libros registrados
+                    3 - Enlistar autores registrados
+                    4 - Enlistar Autores vivos en un año determinado
+                    5 - Enlistar libros por Idioma
+                                                     
+                    0 - Salir
+                    """;
+
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    buscarLibroPorTitulo();
+                    break;
+                case 2:
+                    buscarLibrosRegistrados();
+                    break;
+                case 3:
+                    buscarAutoresRegistrados();
+                    break;
+                case 4:
+                    buscarAutoresVivosPorAno();
+                    break;
+                case 5:
+                    buscarLibroPorIdioma();
+                    break;
+
+                case 0:
+                    System.out.println("Gracias por usar LiterAlura. BYE.");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+
+
+        }
+    }
 }
